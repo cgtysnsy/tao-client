c05605403b6a68fa549810fe31ab534ecf635d16
cagatay <cgtyeo@gmail.com>
2023-03-24 17:33:20 +0300

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, NavbarBrand } from "react-bootstrap";
+import { SocialIcon } from "react-social-icons";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar
+        className="my-2"
+        color="dark"
+        dark
+        style={{ padding: "1rem", margin: "1rem" }}
+      >
+        <NavbarBrand href="/">
+          <SocialIcon
+            url="https://www.instagram.com/tao.eatdrink/"
+            fgColor="gray"
+            bgColor="transparent"
+            style={{ height: 45, width: 45 }}
+            target="_blank"
+          />
+        </NavbarBrand>
+      </Navbar>
+      <Navbar className="m-6" color="secondary" dark></Navbar>
+      <Navbar className="my-2" color="dark" dark>
+        <NavbarBrand href="/"></NavbarBrand>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
