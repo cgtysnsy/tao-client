d53299a93536fd7e7fceef62b5e93280d3e38210
cagatay <cgtyeo@gmail.com>
2023-05-16 23:16:15 +0300

    "sollving login issue"

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -1,3 +1,72 @@
+// import axios from "axios";
+// import React, { useState } from "react";
+// import Cookies from "universal-cookie";
+// const cookies = new Cookies();
+
+// function App() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [login, setLogin] = useState(false);
+
+//   const loginUser = async (event) => {
+//     // prevent the form from refreshing the whole page
+//     event.preventDefault();
+
+//     // set configurations
+//     const configuration = {
+//       method: "post",
+//       url: "https://taoserver.onrender.com/login",
+//       data: {
+//         email,
+//         password,
+//       },
+//     };
+//     console.log("configuration", configuration);
+//     // make the API call
+//     axios(configuration)
+//       .then((result) => {
+//         console.log("🚀 ~ file: Loginpage.js:27 ~ .then ~ result", result);
+//         setLogin(true);
+//         // set the cookie
+//         cookies.set("TOKEN", result.data.token, {
+//           path: "/",
+//         });
+//         // redirect user to the auth page
+//         window.location.href = "/adminpage";
+//       })
+//       .catch((message) => {
+//         message = new Error();
+//         setLogin(false);
+//       });
+//   };
+//   return (
+//     <div className="login-container">
+//       <img src="./images/tao.jpg" alt="tao" className="tao-image login-image" />
+//       <form onSubmit={loginUser} className="login-form">
+//         <input
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           type="email"
+//           placeholder="Email"
+//           className="login-input"
+//         />
+//         <br />
+//         <input
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           type="password"
+//           placeholder="Password"
+//           className="login-input"
+//         />
+//         <br />
+//         <input type="submit" value="Login" className="login-button" />
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import axios from "axios";
 import React, { useState } from "react";
 import Cookies from "universal-cookie";
@@ -15,13 +84,12 @@ function App() {
     // set configurations
     const configuration = {
       method: "post",
-      url: "https://taoserver.onrender.com/login",
+      url: "https://ap-south-1.aws.data.mongodb-api.com/app/user-spdgq/endpoint/login",
       data: {
         email,
         password,
       },
     };
-    console.log("configuration", configuration);
     // make the API call
     axios(configuration)
       .then((result) => {
@@ -32,7 +100,7 @@ function App() {
           path: "/",
         });
         // redirect user to the auth page
-        window.location.href = "/adminpage";
+        //window.location.href = "/adminpage";
       })
       .catch((message) => {
         message = new Error();
@@ -40,15 +108,14 @@ function App() {
       });
   };
   return (
-    <div className="login-container">
-      <img src="./images/tao.jpg" alt="tao" className="tao-image login-image" />
-      <form onSubmit={loginUser} className="login-form">
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginUser}>
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="email"
           placeholder="Email"
-          className="login-input"
         />
         <br />
         <input
@@ -56,10 +123,9 @@ function App() {
           onChange={(e) => setPassword(e.target.value)}
           type="password"
           placeholder="Password"
-          className="login-input"
         />
         <br />
-        <input type="submit" value="Login" className="login-button" />
+        <input type="submit" value="Login" />
       </form>
     </div>
   );
